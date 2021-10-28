2b52163fafd39856d4466ea4e51df4db9c73b0ad
WHJR-Grade12 <91465322+WHJR-G12-Github@users.noreply.github.com>
2021-10-28 20:29:45 +0530

    Create template_c8_saa2.py

---
+++ template_c8_saa2.py
@@ -0,0 +1,10 @@
+# Define a function 'power()' with 'a', 'b' as parameters
+
+    # Calculate 'result' using power operator
+    
+    # Return the 'result'
+    
+
+  
+# Call the function 'power()' with '2' and '3' as arguments 
+# and print the result
